17cdb46acfec5fd8d3b79032dd4df788f22bb8a3
ricwed07 <richdweed@gmail.com>
2026-09-04 14:43:31 -0300

    Update gerador.py

--- gerador.py
+++ gerador.py
@@ -1566,6 +1566,16 @@ HTML_TEMPLATE = """
                             <option value="2026">2026</option><option value="2027">2027</option>
                             <option value="2028">2028</option><option value="2029">2029</option>
                             <option value="2030">2030</option><option value="2031">2031</option>
+                            <option value="2032">2032</option><option value="2033">2033</option>
+                            <option value="2034">2034</option><option value="2035">2035</option>
+                            <option value="2036">2036</option><option value="2037">2037</option>
+                            <option value="2038">2038</option><option value="2039">2039</option>
+                            <option value="2040">2040</option><option value="2041">2041</option>
+                            <option value="2042">2042</option><option value="2043">2043</option>
+                            <option value="2044">2044</option><option value="2045">2045</option>
+                            <option value="2046">2046</option><option value="2047">2047</option>
+                            <option value="2048">2048</option><option value="2049">2049</option>
+                            <option value="2050">2050</option>
                         </select>
                     </div>
                     <div class="card-input-group">
@@ -2090,6 +2100,7 @@ if __name__ == '__main__':
     print("   HTTP://LOCALHOST:5000")
     print("   - ABA GERADOR: GERAR CARDS")
     print("   - ABA VERIFICADOR: VERIFICAR BIN | BUSCA AVANÇADA")
+    print("   - ANOS DISPONÍVEIS: 2026 ATÉ 2050")
     print()
     print("💡 PRESSIONE CTRL+C PARA PARAR")
     print("=" * 60)
